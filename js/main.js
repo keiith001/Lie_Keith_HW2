246a54dd7cf0acd307fdcbc78a7b02d8e1ff8765
keiith001 <kith.richrd@gmail.com>
2025-03-13 17:45:18 -0400

    Adding Bounce Transition
    This commit is to add Bounce Transition with GSAP to add some effects.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 console.log("JS is Connected");
 
 import {Gryffindor, Hufflepuff, Ravenclaw, Slytherin} from "./houses.js";
+import { bounceTransition } from "./bounce.js";
+
 
 const houses = [new Gryffindor(), new Hufflepuff(), new Ravenclaw(), new Slytherin()];
 
 const housesList = document.querySelector("#houses-list");
-const housesDetails = document.querySelector("#houses-details");
 
 houses.forEach((house) => {
     const li = document.createElement("li");
-    li.classList.add("house-item");
+    li.classList.add("houses-item");
 
     const crest = house.generateCrest();
     li.appendChild(crest);
@@ -23,9 +24,24 @@ houses.forEach((house) => {
     li.appendChild(details);
 
     // Add click event to show details
-    crest.addEventListener("click", () => {
-        const visible = details.style.display === "block";
-        details.style.display = visible ? "none" : "block";
+    crest.addEventListener("click", (e) => {
+        const housesItems = document.querySelectorAll(".houses-item");
+        e.stopPropagation();
+
+        // Remove bounce class from all items
+        housesItems.forEach(item => {
+            item.classList.remove("bounce");
+        });
+
+        // Add Bounce when click
+        li.classList.add("bounce");
+
+        if (details.style.display === "none") {
+            details.style.display = "block";
+            bounceTransition(crest);
+        } else {
+            details.style.display = "none";
+        }
     });
 
     housesList.appendChild(li);
